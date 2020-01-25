bbe216c4c06f0d7f95e27b16d27a7bdec1c711fa
Seth <ssthor22@gmail.com>
2020-01-24 19:37:04 -0500

    added all unity files for project

---
+++ Card Game/Assets/Scripts/CardFlipper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFlipper : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    //CardModel model;
+
+    public AnimationCurve scaleCurve;
+    public float duration = 0.5f;
+}
